add80753ce7f418566f04fb4e11fc329875767d6
Gustav Wennerblom <gustav.wennerblom@gmail.com>
2017-03-04 22:35:49 +0100

    Minor refactoring and PEP-8 corrections.

--- DBsetup.py
+++ DBsetup.py
@@ -3,5 +3,5 @@
 import sqlite3
 conn = sqlite3.Connection("submisisons.db")
 cur = conn.cursor()
-cur.execute("CREATE TABLE Test_Submissions (Id INTEGER PRIMARY KEY, Filename TEXT, Submitter TEXT, Region TEXT, Date TEXT, Attachment_Id TEXT, Attachment_Binary BLOB)")
+cur.execute("CREATE TABLE IF NOT EXISTS Test_Submissions (Id INTEGER PRIMARY KEY, Filename TEXT, Submitter TEXT, Region TEXT, Date TEXT, Attachment_Id TEXT, Attachment_Binary BLOB)")
 
--- getcalcs.py
+++ getcalcs.py
@@ -7,30 +7,17 @@ from DBhelper import DBHelper
 from exchangelib import Account, Credentials, DELEGATE
 from excel_parser import ExcelParser
 
-# Credentials for access to mailbox
-# Ignore if in debug mode when working with a spoof message
-if not config.debug:
-    with open("CREDENTIALS.json") as j:
-        text = j.readline()
-        d = json.loads(text)
-    credentials = Credentials(username=d["UID"], password=d["PWD"])
-
-# Referencing Exchange account to fetch submissions from the projectproposal mailbox
-# Ignore if in debug mode when working with a spoof message
-if not config.debug:
-    account = Account(primary_smtp_address="projectproposals@business-sweden.se", credentials=credentials,
-                      autodiscover=True, access_type=DELEGATE)
-
 db = DBHelper()
 
+
 # Function to return a folder given a folder name
-def get_folder(name):
+def get_folder(name, account):
     return account.inbox.get_folder_by_name(name)
 
 
 # Counts number of submissions in a given folder
-def count_submissions_by_region(folder_name):
-    f = get_folder(folder_name)
+def count_submissions_by_region(folder_name, account):
+    f = get_folder(folder_name, account)
     counter = 0
     for item in f.all():
         counter += 1
@@ -45,6 +32,7 @@ def store_attachment(filename, submitter, region, date, checksum, attachment):
 
 
 # Downloads all attachments in a folder into a database, checking for duplicates
+# Possibly not needed? Overridden by 'store_attachment'?
 def download_attachments(folder_name):
     # f = get_folder(folder_name)
     # for item in f.all():
@@ -53,13 +41,13 @@ def download_attachments(folder_name):
 
 
 # Test method to try attachment download
-def download_one_attachment(folder_name):
-    f = get_folder(folder_name)
+def download_one_attachment(folder_name, account):
+    f = get_folder(folder_name, account)
     all_items = []
     for item in f.all():
         all_items.append(item)
 
-    #Drop to shell to test code
+    # Drop to shell to test code
     import code
     code.interact(local=locals())
 
@@ -68,7 +56,6 @@ def download_one_attachment(folder_name):
     # region = all_items[0].subject[:4]
     # date = all_items[0].datetime_received  # need to find the __tostring__ function
     # attachment_id = all_items[0].attachments[0].attachment_id
-
 
 
 # Downloads an attachment into database given a subject line
@@ -82,10 +69,11 @@ def generate_hash(attachment):
 
 
 # Basic run function to print a count of submissions by region
-def count_all():
+def count_all(account):
     regions = ["Americas", "APAC", "MEA", "CEE", "Western Europe"]
     for x in regions:
-        print "%s: %i submissions" % (x, count_submissions_by_region(x))
+        print "%s: %i submissions" % (x, count_submissions_by_region(x, account))
+
 
 # Subrouting for testing. Inserts a spoof message into the database
 def insert_testmessage():
@@ -105,11 +93,12 @@ def insert_testmessage():
     else:
         print "Subroutine not intended for production use. Set debug=True in config.py to use"
 
+
 # Reviews an xlsm file in the database and prints to stdout a set of data about it
 def analyze_submission(db_id):
-    tempfile=db.get_file_by_id(db_id)
+    tempfile = db.get_file_by_id(db_id)
     print 'Temporary file "%s" created' % tempfile
-    parser=ExcelParser(tempfile)
+    parser = ExcelParser(tempfile)
     print "Lead office: %s" % parser.get_lead_office()
     print "Project margin: %s" % parser.get_margin()
     print "Total fee: %s" % parser.get_project_fee()
@@ -119,19 +108,37 @@ def analyze_submission(db_id):
     print "Blended hourly rate: %s" % parser.get_blended_hourly_rate()
     print "Pricing method: %s" % parser.assess_pricing_method()
     # Drop to shell to test code
-    #import code
-    #code.interact(local=locals())
+    # import code
+    # code.interact(local=locals())
 
 
+def main():
 
-if config.debug:
-    #USE BELOW TO INSERT A TEST MESSAGE IN THE DATABASE (CONFIGURE TEST MESSAGE IN MyMessage.py
-    #insert_testmessage()
+    # Credentials for access to mailbox
+    # Ignore if in debug mode when working with a spoof message
+    if not config.debug:
+        with open("CREDENTIALS.json") as j:
+            text = j.readline()
+            d = json.loads(text)
+        credentials = Credentials(username=d["UID"], password=d["PWD"])
 
-    #USE BELOW TO CHECK XLSM ANALYTICS
-    #analyze_submission(18)
+    # Referencing Exchange account to fetch submissions from the projectproposal mailbox
+    # Ignore if in debug mode when working with a spoof message
+    if not config.debug:
+        account = Account(primary_smtp_address="projectproposals@business-sweden.se", credentials=credentials,
+                          autodiscover=True, access_type=DELEGATE)
 
-else:
-    print("Entering live mode with connection to Exchange server")
-    download_one_attachment("Americas")
-    #print("No production lines present")
+    if config.debug:
+        pass
+        # USE BELOW TO INSERT A TEST MESSAGE IN THE DATABASE (CONFIGURE TEST MESSAGE IN MyMessage.py
+        # insert_testmessage()
+
+        # USE BELOW TO CHECK XLSM ANALYTICS
+        # analyze_submission(18)
+    else:
+        print("Entering live mode with connection to Exchange server")
+        download_one_attachment("Americas", account)
+        # print("No production lines present")
+
+if __name__ == '__main__':
+    main()
