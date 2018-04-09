0880363a9a990d94af4ed9b8a25e3bc9b2f62639
Gustav Wennerblom <gustav.wennerblom@gmail.com>
2018-04-09 11:40:13 +0200

    Removal of old web files

--- appforms.py
+++
@@ -1,6 +0,0 @@
-from flask_wtf import Form
-from wtforms import StringField, SubmitField
-
-class FeecalcForm(Form):
-    freefield = StringField("Put what you want here")
-    submit = SubmitField("Send to archive")
