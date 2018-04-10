747c864df5e03c1daf1126a1f254a4b220e621b9
Gustav Wennerblom <gustav.wennerblom@gmail.com>
2018-04-10 22:31:49 +0200

    Test db on

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ enforce_unique_messages = True
 enforce_unique_files = True
 # Options: "MySQL on TestRig", "MySQL on Azure", "Sqlite3", "MSSQL on Azure"
 database = "MSSQL on Azure"
-test_database = False
+test_database = True
 LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
