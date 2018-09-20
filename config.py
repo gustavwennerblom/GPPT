fb5b5f6c578fc8af627edb6a70840c558f1e8019
gustav(azure-deploy) <gustav.wennerblom@gmail.com>
2018-09-20 20:41:11 +0200

    Changed email

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 import logging
 loglevel = logging.INFO
 LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-log_email_recipient = "gustav.wennerblom@gmail.com"
+log_email_recipient = "gustav.wennerblom@business-sweden.se"
 log_directory = "logs"
 main_log_filename = "main.log"
 
 # Development vs production settings
 # debug = False         # redundant setting
-test_database = True
+test_database = False
 
 # General settings
 enforce_unique_messages = True
