34b9739952f39b364014826b24580edb49de386f
Gustav Wennerblom <gustav.wennerblom@gmail.com>
2018-11-20 20:47:23 +0100

    Docs and test utility for setting environment variables

---
+++ README.md
@@ -0,0 +1,12 @@
+# Global Project Pricing Tool (GPPT) fetcher and analyser
+
+Required environment variables:
+* EWS_USERNAME - username for authentication with Exchange Web Services
+* EWS_PASSWORD - password for authentication with EWS
+* DB_HOST - database hostname (server URL)
+* DB_NAME - database name
+* DB_USER - database username with global read and write rights
+* DB_PASSWORD - password for the database user
+* SMTP_URL - URL to the SMTP server used for distributing log files
+* SMTP_USER - username for SMTP login
+* SMTP_PASSWORD - password for SMTP login
---
+++ test_envvars.py
@@ -0,0 +1,29 @@
+import os
+
+envvars = ['EWS_USERNAME,'
+           'EWS_PASSWORD',
+           'DB_HOST',
+           'DB_NAME',
+           'DB_USER',
+           'DB_PASSWORD',
+           'SMTP_URL',
+           'SMTP_USER',
+           'SMTP_PASSWORD']
+
+
+def print_envvars():
+    for envvar in envvars:
+        print(f'{envvar} is set to {os.environ.get(envvar)}')
+
+
+def check_exist():
+    for envvar in envvars:
+        if not os.environ.get(envvar):
+            # print(f'Missing required environment variable {envvar}')
+            raise OSError(f'Missing required environment variable {envvar}')
+    return True
+
+
+if __name__ == '__main__':
+    if check_exist():
+        print("All required environment variables set")
